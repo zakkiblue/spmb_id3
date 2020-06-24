1d02372a9d015d29df9c8ea6b4286e7d18cf02b5
zakki mahatir <zakkimahatir77@gmail.com>
2020-06-24 20:56:04 +0700

    selesai by Zakki

--- index.php
+++ index.php
@@ -23,31 +23,31 @@
 					<td><input type="text" name="nama"></td>
 				</tr>
 				<tr>
-				<td><label for="tpa_id">TPA</label></td>
-				<td><select name="tpa" id="tpa_id">
-					<option value="baik">Baik</option>
-					<option value="buruk">Buruk</option>
-				</select>
-				</td>
+					<td><label for="tpa_id">TPA</label></td>
+					<td><select name="tpa" id="tpa_id">
+						<option value="baik">Baik</option>
+						<option value="buruk">Buruk</option>
+					</select>
+					</td>
 				</tr>
 				<tr>
-				<td><label for="toefl_id">TOEFL</label></td>
-				<td><select name="toefl" id="toefl_id">
-					<option value="bagus">Bagus</option>
-					<option value="cukup">Cukup</option>
-					<option value="kurang">Kurang</option>
-				</select>
-				</td>
+					<td><label for="toefl_id">TOEFL</label></td>
+					<td><select name="toefl" id="toefl_id">
+						<option value="bagus">Bagus</option>
+						<option value="cukup">Cukup</option>
+						<option value="kurang">Kurang</option>
+					</select>
+					</td>
 				</tr>
 				<tr>
-				<td><label for="un_id">UN</label></td>
-				<td>
-				<select name="un" id="un_id">
-					<option value="tinggi">Tinggi</option>
-					<option value="sedang">Sedang</option>
-					<option value="rendah">Rendah</option>
-				</select>
-				</td>
+					<td><label for="un_id">UN</label></td>
+					<td>
+					<select name="un" id="un_id">
+						<option value="tinggi">Tinggi</option>
+						<option value="sedang">Sedang</option>
+						<option value="rendah">Rendah</option>
+					</select>
+					</td>
 				</tr>
 			</table>
 			<button name="submit" type="submit">Submit</button>
