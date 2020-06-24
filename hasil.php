ef5e16ff9f84d8e999f6a0a29ba7dc4f4d459510
zakki mahatir <zakkimahatir77@gmail.com>
2020-06-24 20:54:15 +0700

    finish

--- hasil.php
+++ hasil.php
@@ -1,1 +1,34 @@
-hasil.php
+<?php
+if (isset($_POST['nama'])) {
+	# code...
+	if ($_POST['tpa'] == 'baik') {
+		$hasil = "Diterima";
+	}else {
+		if ($_POST['toefl'] == 'cukup') {
+			if ($_POST['un'] == 'rendah') {
+				$hasil = "Tidak Diterima";
+			}else {
+				$hasil = "Diterima";
+			}
+			
+		}else {
+			$hasil = "Tidak Diterima";
+		}
+	}
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Hasil SPMB</title>
+</head>
+<body>
+	<div><h1>Hasil Seleksi Penerimaan Mahasiswa Baru</h1></div>
+	<div>
+		<h2> Nama : <?= $_POST['nama']; ?></h2>
+		<h3>Keterangan : <?= $hasil; ?></h3>
+	</div>
+</body>
+</html>
--- index.php
+++ index.php
@@ -2,16 +2,55 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
+	<style>
+		tr {
+			margin: 5px;
+			padding: 0px;
+		}
+		ul {
+			padding : 0px;
+		}
+	</style>
 	<title>ID3 SPMB</title>
 </head>
 <body>
 	<div><h1>Sitem Penerimaan Mahasiwa Baru</h1></div>
 	<div>
 		<form action="hasil.php" method="POST">
-			<ul>
-				<ol><input type="text" name="nama"></ol>
-
-			</ul>
+			<table>
+				<tr>
+					<td><label for="nama">Nama</label></td>
+					<td><input type="text" name="nama"></td>
+				</tr>
+				<tr>
+				<td><label for="tpa_id">TPA</label></td>
+				<td><select name="tpa" id="tpa_id">
+					<option value="baik">Baik</option>
+					<option value="buruk">Buruk</option>
+				</select>
+				</td>
+				</tr>
+				<tr>
+				<td><label for="toefl_id">TOEFL</label></td>
+				<td><select name="toefl" id="toefl_id">
+					<option value="bagus">Bagus</option>
+					<option value="cukup">Cukup</option>
+					<option value="kurang">Kurang</option>
+				</select>
+				</td>
+				</tr>
+				<tr>
+				<td><label for="un_id">UN</label></td>
+				<td>
+				<select name="un" id="un_id">
+					<option value="tinggi">Tinggi</option>
+					<option value="sedang">Sedang</option>
+					<option value="rendah">Rendah</option>
+				</select>
+				</td>
+				</tr>
+			</table>
+			<button name="submit" type="submit">Submit</button>
 		</form>
 	</div>
 	
