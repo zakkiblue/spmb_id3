fe47432ab5b2da635736a834de11525ca5a197ce
zakki mahatir <zakkimahatir77@gmail.com>
2020-06-24 20:28:15 +0700

    pertama

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>ID3 SPMB</title>
+</head>
+<body>
+	<div><h1>Sitem Penerimaan Mahasiwa Baru</h1></div>
+	<div>
+		<form action="hasil.php" method="POST">
+			<ul>
+				<ol><input type="text" name="nama"></ol>
+
+			</ul>
+		</form>
+	</div>
+	
+</body>
+</html>
